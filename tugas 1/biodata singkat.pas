dec74ba34f83cce669705446640a8c5a25246fce
andigunawan1123 <73595034+andigunawan1123@users.noreply.github.com>
2021-03-21 22:10:26 +0700

    tugas 1
    upload tugas

---
+++ tugas 1/biodata singkat.pas
@@ -0,0 +1,20 @@
+program latihan;
+uses crt;
+
+begin
+  clrscr;
+  Write('1. Nama : ');
+  ReadLn();
+  Write('2. NPM : ');
+  ReadLn();
+  Write('3. Alamat : ');
+  ReadLn();
+  Write('4. TTL : ');
+  ReadLn();
+  Write('5. No. HP : ');
+  ReadLn();
+  Write('6. Motto : ');
+  ReadLn();
+  readln;
+
+end.
