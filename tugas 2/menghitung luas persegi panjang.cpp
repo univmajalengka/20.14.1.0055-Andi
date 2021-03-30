79afa52d06a7a26a6027b34aed3aed9fe60bd31a
andigunawan1123 <73595034+andigunawan1123@users.noreply.github.com>
2021-03-30 15:57:26 +0700

    c++

---
+++ tugas 2/menghitung luas persegi panjang.cpp
@@ -0,0 +1,21 @@
+//Nama : AndiGunawan
+//NPM : 201410055
+//Nama program : Tugas3.cpp
+#include<iostream>
+using namespace std;
+
+int main(){
+	int l,p,luas;
+	
+	cout<<"Perogram Luas Persegi Panjang"<<endl;
+	cout<<"============================="<<endl;
+	cout<<"Masukkan nilai Panjang\t:";
+	cin>>p;
+	cout<<"Masukkan nilai Lebar\t:";
+	cin>>l;
+	cout<<endl;
+	luas=p*l;
+	cout<<"Luas = Panjang x Lebar"<<endl;
+	cout<<endl;
+	cout<<"Luas\t= "<<luas;
+}
