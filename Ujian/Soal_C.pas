a3ac8edc1fcfb65b8abd4a946006bd66088393bb
andigunawan1123 <73595034+andigunawan1123@users.noreply.github.com>
2021-04-22 15:19:25 +0700

    Upload soal_c

---
+++ Ujian/Soal_C.pas
@@ -0,0 +1,61 @@
+program data_usia_mahasiswa;
+uses crt;
+const
+ Nmin = 1;
+ Nmax = 10;
+type
+domain = Nmin..Nmax;
+tnilai = record
+nama : string;
+tlahir : integer;
+usia : integer;
+end;
+
+var
+dusia : array[domain] of tnilai;
+N : integer;
+
+procedure batas1;
+ begin
+  writeln('------------------------------------------------------');
+ end;
+
+procedure batas2;
+ begin
+  writeln('======================================================');
+ end;
+
+//input
+procedure inputdata(var N:integer);
+ var
+   i,j: integer;
+ begin
+     for i := 1 to N do
+      begin
+       writeln('Data Mahasiswa ke-',i,':');
+       write('Masukkan Nama : ');
+       readln(dusia[i].nama);
+       write('Masukkan Tahun Lahir (2021) : ');
+       readln(dusia[i].tlahir);
+       dusia[i].usia :=  2021 -  dusia[i].tlahir;
+       batas2;
+       writeln('Usia ',dusia[i].nama,' adalah ',dusia[i].usia,' tahun');
+       batas1;
+       writeln;
+      end;
+    end;
+
+
+//main
+begin
+ clrscr;
+  writeln('PROGRAM PERHITUNGAN USIA');
+  batas2;
+ write('Masukkan Jumlah Data Mahasiswa : ');
+ readln(N);
+ batas1;
+ inputdata(N);
+ readln;
+end.
+
+
